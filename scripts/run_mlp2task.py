0e80d3a9356866ea6ed6c933c3f434d9ac67459b
Youssef Kashef <youssef.kashef@gmail.com>
2018-01-06 21:28:13 +0100

    fix branch default

--- scripts/run_mlp2task.py
+++ scripts/run_mlp2task.py
@@ -81,7 +81,7 @@ def run(run_name, args):
                 'n_nodes'       : cfg['n_nodes'],
                 'n_input'       : n_input,
                 'prefix'        : cfg['prefix'],
-                'branch'        : cfg.get('branch', len(cfg['n_nodes'])-1),
+                'branch'        : cfg.get('branch', len(cfg['n_nodes'])-2), # substract additional because of decision layer
                 'logger_name'   : cfg['logger_name'],
                 }
             net = MLP(classifier_params)
